98cb3877e25691c0fd7888abc4e2603ed37cfec5
kalindimchev <kalin.dimchev@gmail.com>
2019-04-06 23:33:02 +0300

    sdfasdfasd

--- merge shits/HoiProject/Program.cs
+++ merge shits/HoiProject/Program.cs
@@ -10,8 +10,8 @@ namespace HoiProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tashaci");
-            Console.WriteLine("madja");
+            Console.WriteLine("hoi tashaci");
+            Console.WriteLine("hoi madja");
         }
     }
 }
